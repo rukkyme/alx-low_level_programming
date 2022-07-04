52c7c2615c7defc554bca9fef59021934a31405a
Joy <communicatejoy@yahoo.com>
2022-07-04 13:13:33 -0700

    My 0-positive_or_negative task commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,11 +20,13 @@ printf("%i is zero\n", n);
 }
 
 else if (n<0)
-{printf("%i is negative\n", n);
+{
+printf("%i is negative\n", n);
 }
 
 else
-{printf("%i is poditive\n", n);
+{
+printf("%i is poditive\n", n);
 }
 return (0);
 
