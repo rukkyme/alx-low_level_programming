e5154501bb093922d2f4a9839ae225d2de0ce6ae
Joy <communicatejoy@yahoo.com>
2022-07-10 07:57:19 -0700

    my zero task revisit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
  * Description: prints if number is positive or negative
@@ -19,7 +20,8 @@ if (n == 0)
 printf("%i is zero\n", n);
 }
 
-else if (n < 0)
+else 
+if (n < 0)
 {
 printf("%i is negative\n", n);
 }
