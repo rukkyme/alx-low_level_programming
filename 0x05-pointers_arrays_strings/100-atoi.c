62f0fd58c81de972a934c389c427d779dc5464af
Joy <communicatejoy@yahoo.com>
2022-07-13 06:28:43 -0700

    tenth task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,7 +12,7 @@ int _atoi(char *s)
 	unsigned int num;
 	char *temp;
 
-	temp = s
+	temp = s;
 	num = 0;
 	sign = 1;
 	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
@@ -24,9 +24,9 @@ int _atoi(char *s)
 	if (*temp != '\0')
 	{
 		do {
-			num =num * 10 + (*te,[ - '0');
+			num = num * 10 + (*temp - '0');
 			temp++;
-		} while (*temp >= '0' && "temp <= '9');
+		} while (*temp >= '0' && *temp <= '9');
 	}
 	return (num * sign);
 }
