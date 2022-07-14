da25bdc58af454319dcd8cdaae7491ccfbbbfacd
Joy <communicatejoy@yahoo.com>
2022-07-14 04:48:55 -0700

    my zero task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,25 @@
 #include "main.h"
+
 /**
- *_strcat - concatenates two strings
- *@dest: This is a string that will be appended
- *@src: A pointter to a character that will also be changed
- *Return: dest
+ * _strcat - concatenates two strings
+ * @dest: This is a string that will be appended
+ * @src: A pointter to a character that will also be changed
+ * Return: dest
  */
 
 char *strcat(char *dest, char *strc)
 {
-	int index = 0, dest_len = 0;
+	int index = 0;
+      	int dest_len = 0;
 	
 	while (dest[index++])
 		dest_len++;
-
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src{index};
-
 	return (dest);
 }
+
+
 
 
 
