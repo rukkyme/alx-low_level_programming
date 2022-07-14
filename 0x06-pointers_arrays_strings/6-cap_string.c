503ec3b6fe8b1d0869b195b76e438c2d28ceb9ac
Joy <communicatejoy@yahoo.com>
2022-07-14 16:13:08 -0700

    my zero task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -29,13 +29,14 @@ char *cap_string(char *str)
 			str[index - 1] == '{' ||
 			str[index - 1] == '}' ||
 			index == 0)
-				str[index] -= 32;
+			    str[index] -= 32;
 
 		index++;
 	}
 
 	return (str);
 }
+
 
 
 		
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,22 +2,24 @@
 
 /**
  * leet - Encodes a string into 1337
- * @s: The string
+ * @str: The string
  *
  * Return: The string
  */
-char *leet(char *s)
+char *leet(char *str)
 {
 	int i, j;
 	char subs[] = "aAeEoOtTlL";
-	char le[] = "43071";
+	char leet[10] = "43071";
 
-	i = 0
-	for (i = 0; s[i] != '\0'; j++)
-		if (s[i] == subs[j])
-			s[i] = le[j / 2];
+	i = 0;
+	for (i = 0; str[i] != '\0'; j++)
+	{
+		if (str[i] == subs[j])
+			str[i] = leet[j / 2];
 	}
 
-	return (s);
+	return (str);
 }
+
 
