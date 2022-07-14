15d65a7122c31fc8138c94ff7ff8bf14cbe0530b
Joy <communicatejoy@yahoo.com>
2022-07-14 14:12:48 -0700

    my fifth task

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,12 +6,11 @@
  *  Return: returns character
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
 	int index = 0;
 
-	while (str[index]);
-
+	while (str[index])
 	{
 		if (str[index] >= 'a' && str[index] <= 'z')
 			str[index] -= 32;
