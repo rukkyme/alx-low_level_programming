95a27caa58622420f77b54a4120474aabfc3e51a
Joy <communicatejoy@yahoo.com>
2022-07-17 14:46:03 -0700

    my 6-print_numberz.c task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+
+	{
+		printf("%", i);
+	}
+
+	printf("\n");
+
+		return (0);
+
+}
+
