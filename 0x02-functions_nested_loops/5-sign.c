e0cf30febfd7e58aced9248a8eaaf88c5595fb66
Joy <communicatejoy@yahoo.com>
2022-09-14 07:00:00 -0700

    My fifth task commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign - function checks sign on numbers
+ * @n:This is the input number
+ * Description: this will print the sig of a number
+ * Return:1 if number is +tive,0 if number is zero, -1 if number is negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+
+	else if (n < 0)
+	{
+	_putchar('-');
+	return (-1);
+	}
+
+	else if (n == 0)
+	{
+	_putchar('0');
+	return (0);
+	}
+
+}
+
+
+
