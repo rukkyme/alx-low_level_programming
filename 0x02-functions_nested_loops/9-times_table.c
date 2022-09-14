d7fcf9439f698c487e2c12978d9069daf7ffd685
Joy <communicatejoy@yahoo.com>
2022-09-14 14:13:23 -0700

    learn the times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,33 +7,22 @@
  */
 void times_table(void)
 {
-	int i, j, n;
+	int num, mult, prod;
 
-	for (i = 0; i <= 9; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+		for (mult = 1; mult <= 9; mult++)
 		{
-			n = i * j;
-			if ((n / 10) == 0)
-			{
-				if (j != 0)
-				_putchar(' ');
-				_putchar(n + '0');
+			_putchar(',');
+			_putchar(' ');
 
-				if (j == 9)
-				_putchar(',');
+			prod = num * mult;
+			if (prod <= 9)
 				_putchar(' ');
-
-			}
 			else
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-
-				if (j == 9)
-				_putchar(',');
-				_putchar(' ');
-			}
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
