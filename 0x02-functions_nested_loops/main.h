d360b9d3ee98e037a9aa1a665c6234bbe2681189
Joy <communicatejoy@yahoo.com>
2022-09-13 19:26:34 -0700

    My putchar task commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef "main.h"
-#define "main.h"
+#ifndef main_h
+#define main_h
 
 int _putchar(char);
 void print_alphabet(void);
@@ -15,4 +15,5 @@ int add(int, int);
 void print_to_98(int n);
 
 #endif
+
 
