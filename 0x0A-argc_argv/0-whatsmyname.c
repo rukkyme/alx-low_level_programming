fe24f156f5a8870df824768133e56cafacb316b2
Joy <communicatejoy@yahoo.com>
2022-10-02 17:36:36 -0700

    zero task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this prints the name of the prograam 
+ * @argc: number of arguments passed to the function
+ * @argv: the argument vector of pointers to string
+ * Return: always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
