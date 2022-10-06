8c5cb6ca45d25391283b2cc3249111d48492e38d
Joy <communicatejoy@yahoo.com>
2022-10-06 10:30:56 -0700

    my zero task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - this allocates memory using malloc
+ * @b: memory to allocate
+ *
+ * Return: the pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
+
