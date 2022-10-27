60cbc448948849c4fc4e513e2d2519369dd989d7
Joy <communicatejoy@yahoo.com>
2022-10-27 14:56:53 -0700

    zero task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,4 @@
-#include <stddef.h>
 #include "main.h"
-
-
-/**
- * check_valid_string - checks if a string has only 0's and 1's
- * @b: string to be checked
- *
- * Return: 1 if string is valid, 0 otherwise
- */
-int check_valid_string(const char *b)
-{
-	if (b == NULL)
-		return (0);
-	while (*b)
-	{
-		if (*b != '1' && *b != '0')
-			return (0);
-		b++;
-	}
-	return (1);
-}
 
 /**
  * binary_to_uint - converts a binary to an unsigned int
